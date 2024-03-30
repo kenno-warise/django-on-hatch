a5dfc6ac8c9e2028e596e9cb472516a18c58d644
Kenno-Warise <wariken0523@gmail.com>
2024-03-30 12:50:57 +0900

    lintモジュールも引数からpyproject.tomlのテーブル内の定義にする

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path  # type: ignore
 from . import views
 
 
 urlpatterns = [
-        path('', views.AppPageView.as_view(), name='index')
+    path('', views.AppPageView.as_view(), name='index')
+    # path('example/', views.Example.as_view(), name='example')
 ]
-
